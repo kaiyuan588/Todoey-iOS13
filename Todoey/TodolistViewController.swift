c1977551d48f45ac901d7d0f5c305b14bf228b54
kaiyuan zhao <kaiyuan941014@gmail.com>
2021-01-29 18:18:56 -0500

    initial todo list

--- Todoey/TodolistViewController.swift
+++ Todoey/TodolistViewController.swift
@@ -9,11 +9,24 @@
 import UIKit
 
 class TodolistViewController: UITableViewController {
+    
     let itemArray = ["Skateboarding", "Onewheeling", "Snowboarding"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
 
 }
-
